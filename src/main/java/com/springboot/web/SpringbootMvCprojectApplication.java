63d84ceea28373add5a7769fc9944752fc379ade
Rohit Mittal <43905130+rohitm131@users.noreply.github.com>
2024-01-09 13:15:39 +0530

    Add files via upload

---
+++ src/main/java/com/springboot/web/SpringbootMvCprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvCprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvCprojectApplication.class, args);
+	}
+
+}
